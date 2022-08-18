dc8f19e77f1765f0e28395e5224cf07c1c4e6118
Donteatpineappleonpizza <winittra_saengsroy@cmu.ac.th>
2022-08-18 17:43:05 +0700

    fn

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,64 @@ import {
 } from "@tabler/icons";
 
 export default function Home() {
-  const deleteTodo = (idx) => {};
+  const [todo, setTodo] = useState([]);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+      return;
+    }
+    const todoStr = JSON.stringify(todo);
+    localStorage.setItem("todo-react", todoStr);
+  }, [todo]);
 
-  const markTodo = (idx) => {};
+  useEffect(() => {
+    const todoStr = localStorage.getItem("todo-react");
+    if (!todoStr) setTodo([]);
+    else setTodo(JSON.parse(todoStr));
+  }, []);
 
-  const moveUp = (idx) => {};
+  const deleteTodo = (idx) => {
+    todo.splice(idx, 1);
+    const newTodo = [...todo];
+    setTodo(newTodo);
+  };
 
-  const moveDown = (idx) => {};
+  const markTodo = (idx) => {
+    todo[idx].completed = !todo[idx].completed;
+    setTodo([...todo]);
+  };
+
+  const moveUp = (idx) => {
+    if (idx == 0) return;
+    let temp = todo[idx];
+    todo[idx] = todo[idx - 1];
+    todo[idx - 1] = temp;
+    setTodo([...todo]);
+  };
+
+  const moveDown = (idx) => {
+    if (idx == todo.length - 1) return;
+    let temp = todo[idx];
+    todo[idx] = todo[idx + 1];
+    todo[idx + 1] = temp;
+    setTodo([...todo]);
+  };
+
+  const addTodo = (title, completed) => {
+    setTodo([{ title: title, completed: completed }, ...todo]);
+  };
+
+  const handleInput = (event) => {
+    if (event.key == "Enter") {
+      if (event.target.value === "") {
+        alert("Todo cannot be empty");
+        return;
+      }
+      addTodo(event.target.value, false);
+      event.target.value = "";
+    }
+  };
 
   return (
     <div>
@@ -28,13 +79,27 @@ export default function Home() {
         <input
           className="form-control mb-1 fs-4"
           placeholder="insert todo here..."
+          onKeyUp={handleInput}
         />
+
+        {todo.map((element, i) => (
+          <Todo
+            key={i}
+            title={element.title}
+            completed={element.completed}
+            onMark={() => markTodo(i)}
+            onDelete={() => deleteTodo(i)}
+            onMoveUp={() => moveUp(i)}
+            onMoveDown={() => moveDown(i)}
+          />
+        ))}
+
         {/* Todos */}
-        {/* Example 1 */}
+        {/* Example 1 
         <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
           <span className="me-auto">Todo</span>
-        </div>
-        {/* Example 2 */}
+        </div>*/}
+        {/* Example 2 
         <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
           <span className="me-auto">Todo with buttons</span>
 
@@ -50,18 +115,22 @@ export default function Home() {
           <button className="btn btn-danger">
             <IconTrash />
           </button>
-        </div>
+        </div>*/}
 
         {/* summary section */}
         <p className="text-center fs-4">
-          <span className="text-primary">All (2) </span>
-          <span className="text-warning">Pending (2) </span>
-          <span className="text-success">Completed (0)</span>
+          <span className="text-primary">All ({todo.length}) </span>
+          <span className="text-warning">
+            Pending ({todo.filter((e) => e.completed == false).length})
+          </span>
+          <span className="text-success">
+            Completed ({todo.filter((e) => e.completed == true).length})
+          </span>
         </p>
 
         {/* Made by section */}
         <p className="text-center mt-3 text-muted fst-italic">
-          made by Chayanin Suatap 12345679
+          made by Winittra Saengsroy 640612097
         </p>
       </div>
     </div>
